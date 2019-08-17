7aa8542e0f9945e7c9da640747b3b9eff418e0fe
Khaja Moinuddin Mohammed <moinuddinbtech@gmail.com>
2019-08-18 00:21:04 +0530

    Initial Commit

---
+++ src/main/java/com/iblockdev/travis/ci/TravisCIDemo/TravisCiDemoApplication.java
@@ -0,0 +1,13 @@
+package com.iblockdev.travis.ci.TravisCIDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravisCiDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TravisCiDemoApplication.class, args);
+	}
+
+}
